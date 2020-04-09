fcb52dbb327a65b8f2b449da7a28f15eb9609e38
lordcifer <theaaw@gmail.com>
2020-04-09 11:17:51 -0400

    Se enlazo la entrada de mercancias con stock y se modifico el diseno

--- SistemaFacturaciones/SistemaFacturaciones/Controllers/EntradaMercanciasController.cs
+++ SistemaFacturaciones/SistemaFacturaciones/Controllers/EntradaMercanciasController.cs
@@ -8,33 +8,11 @@ using System.Web;
 using System.Web.Mvc;
 using SistemaFacturaciones.Models;
 
-namespace SistemaFacturaciones.Controllers
+namespace SistemaFacturacion.Controllers
 {
     public class EntradaMercanciasController : Controller
     {
         private SF db = new SF();
-
-        // GET: EntradaMercancias
-        public ActionResult Index()
-        {
-            var entradaMercancias = db.EntradaMercancias.Include(e => e.Producto).Include(e => e.Proveedore);
-            return View(entradaMercancias.ToList());
-        }
-
-        // GET: EntradaMercancias/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
-            if (entradaMercancia == null)
-            {
-                return HttpNotFound();
-            }
-            return View(entradaMercancia);
-        }
 
         // GET: EntradaMercancias/Create
         public ActionResult Create()
@@ -51,77 +29,29 @@ namespace SistemaFacturaciones.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,producto_id,proveedor_id,Cantidad,Fecha")] EntradaMercancia entradaMercancia)
         {
+            var existenciaStock = db.Stocks.SingleOrDefault(s => s.producto_id == entradaMercancia.producto_id);
+
             if (ModelState.IsValid)
             {
+                if (existenciaStock != null)
+                    db.Stocks.SingleOrDefault(s => s.Id == existenciaStock.Id).Cantidad += entradaMercancia.Cantidad;
+                else
+                    db.Stocks.Add(new Stock()
+                    {
+                        Cantidad = entradaMercancia.Cantidad,
+                        producto_id = entradaMercancia.producto_id
+                    });
+
+                entradaMercancia.Fecha = DateTime.Now;
                 db.EntradaMercancias.Add(entradaMercancia);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+
+                return RedirectToAction("Index", "Home");
             }
 
             ViewBag.producto_id = new SelectList(db.Productos, "Id", "Nombre", entradaMercancia.producto_id);
             ViewBag.proveedor_id = new SelectList(db.Proveedores, "Id", "Nombre", entradaMercancia.proveedor_id);
             return View(entradaMercancia);
-        }
-
-        // GET: EntradaMercancias/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
-            if (entradaMercancia == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.producto_id = new SelectList(db.Productos, "Id", "Nombre", entradaMercancia.producto_id);
-            ViewBag.proveedor_id = new SelectList(db.Proveedores, "Id", "Nombre", entradaMercancia.proveedor_id);
-            return View(entradaMercancia);
-        }
-
-        // POST: EntradaMercancias/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,producto_id,proveedor_id,Cantidad,Fecha")] EntradaMercancia entradaMercancia)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(entradaMercancia).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.producto_id = new SelectList(db.Productos, "Id", "Nombre", entradaMercancia.producto_id);
-            ViewBag.proveedor_id = new SelectList(db.Proveedores, "Id", "Nombre", entradaMercancia.proveedor_id);
-            return View(entradaMercancia);
-        }
-
-        // GET: EntradaMercancias/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
-            if (entradaMercancia == null)
-            {
-                return HttpNotFound();
-            }
-            return View(entradaMercancia);
-        }
-
-        // POST: EntradaMercancias/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
-            db.EntradaMercancias.Remove(entradaMercancia);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
