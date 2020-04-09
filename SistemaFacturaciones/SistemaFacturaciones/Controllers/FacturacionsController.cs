ebf294edc7ea3d6352a61761eaa76f9e2365d65c
lordcifer <theaaw@gmail.com>
2020-04-09 11:24:40 -0400

    Se modifico el diseno de Facturacion

--- SistemaFacturaciones/SistemaFacturaciones/Controllers/FacturacionsController.cs
+++ SistemaFacturaciones/SistemaFacturaciones/Controllers/FacturacionsController.cs
@@ -8,116 +8,47 @@ using System.Web;
 using System.Web.Mvc;
 using SistemaFacturaciones.Models;
 
-namespace SistemaFacturaciones.Controllers
+namespace SistemaFacturacion.Controllers
 {
     public class FacturacionsController : Controller
     {
         private SF db = new SF();
 
-        // GET: Facturacions
-        public ActionResult Index()
-        {
-            var facturacions = db.Facturacions.Include(f => f.Cliente);
-            return View(facturacions.ToList());
-        }
-
-        // GET: Facturacions/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Facturacion facturacion = db.Facturacions.Find(id);
-            if (facturacion == null)
-            {
-                return HttpNotFound();
-            }
-            return View(facturacion);
-        }
-
         // GET: Facturacions/Create
         public ActionResult Create()
         {
-            ViewBag.cliente_id = new SelectList(db.Clientes, "Id", "Cedula");
-            return View();
+            var compras = db.Compras.Include(c => c.Cliente).Include(c => c.Producto);
+
+            ViewBag.cliente_id = new SelectList(db.Clientes, "Id", "Nombre");
+
+            return View(compras.ToList());
         }
 
-        // POST: Facturacions/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,cliente_id,Descuento,Monto,Fecha")] Facturacion facturacion)
+        public ActionResult Create([Bind(Include = "Id,cliente_id,Descuento,Itbis,Monto,Fecha")] Facturacion facturacion)
         {
+            decimal monto = 0, descuento = 0, sumatoria = 0;
+
+            foreach (var item in db.Compras.Where(c => c.cliente_id == facturacion.cliente_id))
+                sumatoria += item.Cantidad * db.Productos.SingleOrDefault(p => p.Id == item.producto_id).Precio;
+
+            descuento = facturacion.descuento(sumatoria, db.Clientes.SingleOrDefault(c => c.Id == facturacion.cliente_id).Categoria);
+            monto = facturacion.itbis(descuento);
+
             if (ModelState.IsValid)
             {
+                facturacion.Descuento = descuento;
+                facturacion.Monto = monto;
+                facturacion.Fecha = DateTime.Now;
+                db.Compras.RemoveRange(db.Compras.ToList());
                 db.Facturacions.Add(facturacion);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Facturaciones", "Consultas");
             }
 
             ViewBag.cliente_id = new SelectList(db.Clientes, "Id", "Cedula", facturacion.cliente_id);
             return View(facturacion);
-        }
-
-        // GET: Facturacions/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Facturacion facturacion = db.Facturacions.Find(id);
-            if (facturacion == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.cliente_id = new SelectList(db.Clientes, "Id", "Cedula", facturacion.cliente_id);
-            return View(facturacion);
-        }
-
-        // POST: Facturacions/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,cliente_id,Descuento,Monto,Fecha")] Facturacion facturacion)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(facturacion).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.cliente_id = new SelectList(db.Clientes, "Id", "Cedula", facturacion.cliente_id);
-            return View(facturacion);
-        }
-
-        // GET: Facturacions/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Facturacion facturacion = db.Facturacions.Find(id);
-            if (facturacion == null)
-            {
-                return HttpNotFound();
-            }
-            return View(facturacion);
-        }
-
-        // POST: Facturacions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Facturacion facturacion = db.Facturacions.Find(id);
-            db.Facturacions.Remove(facturacion);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
--- SistemaFacturaciones/SistemaFacturaciones/Models/Facturacion.cs
+++ SistemaFacturaciones/SistemaFacturaciones/Models/Facturacion.cs
@@ -5,6 +5,7 @@ namespace SistemaFacturaciones.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Facturacion")]
     public partial class Facturacion
@@ -20,5 +21,50 @@ namespace SistemaFacturaciones.Models
         public DateTime? Fecha { get; set; }
 
         public virtual Cliente Cliente { get; set; }
+        public decimal descuento(decimal cantidad, string categoriaCliente)
+        {
+            if (categoriaCliente.ToLower().Equals("premium"))
+                cantidad -= (cantidad * 0.25m);
+
+            return cantidad;
+        }
+
+        public decimal itbis(decimal cantidad)
+        {
+            cantidad += (cantidad * 0.18m);
+
+            return cantidad;
+        }
+
+        public int conteo(List<Facturacion> list)
+        {
+            return list.Count;
+        }
+        public decimal sumatoria(List<Facturacion> list)
+        {
+            return list.Sum(f => f.Monto);
+        }
+
+        public decimal promedio(List<Facturacion> list)
+        {
+            if (list.Count != 0)
+                return list.Average(f => f.Monto);
+            else
+                return 0;
+        }
+        public decimal valorMinimo(List<Facturacion> list)
+        {
+            if (list.Count != 0)
+                return list.Min(f => f.Monto);
+            else
+                return 0;
+        }
+        public decimal valorMaximo(List<Facturacion> list)
+        {
+            if (list.Count != 0)
+                return list.Max(f => f.Monto);
+            else
+                return 0;
+        }
     }
 }
